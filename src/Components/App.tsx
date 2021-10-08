10fbc07b9b68e4cf7e33c22b1915013cef04a152
Ash Ringler <ashleeringler@gmail.com>
2021-10-07 22:52:20 -0600

    base path

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 
 import { Route, Switch } from "react-router";
 
-import { AppWrapper } from "./App.styles";
 import { BrowserRouter } from "react-router-dom";
 import { RepoDetails } from "./RepoDetails";
 import { SearchRepositories } from "./SearchRespositories";
@@ -11,10 +10,10 @@ const App = () => {
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route exact path="/">
+				<Route exact path="/github-repository-search">
 					<SearchRepositories />
 				</Route>
-				<Route path="/:id">
+				<Route path="/github-repository-search/:id">
 					<RepoDetails />
 				</Route>
 			</Switch>
